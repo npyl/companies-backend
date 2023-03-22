7f9c98ee64b6a9ddc26f22a92c8273cb29f952ce
npyl <n.pylarinos@hotmail.com>
2023-03-22 11:54:48 +0200

    Allow our frontend to request us locally

--- src/main/java/org/npyl/companiesbackend/controller/RESTController.java
+++ src/main/java/org/npyl/companiesbackend/controller/RESTController.java
@@ -1,6 +1,7 @@
 package org.npyl.companiesbackend.controller;
 
 import org.npyl.companiesbackend.entity.Company;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class RESTController {
     @GetMapping("/companies-list")
     public List<Company> getCompaniesList()
